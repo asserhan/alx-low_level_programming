cfea5d555f990562d551c1b066e548b0d864b421
asserhan <asseraouhanane@gmail.com>
2023-03-22 01:36:25 +0000

    alph

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char c = 97;
+
+	while ( c <= 122)
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+
+}
